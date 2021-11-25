11181e3c299a345116fdb8c044b3f16df12c5bd4
Katya <ktrnstm@gmail.com>
2021-11-25 17:41:30 +0300

    step2

--- gendiff.js
+++ gendiff.js
@@ -1,4 +1,6 @@
 const program = require('commander');
 program.description(' Compares two configuration files and shows a difference.')
 program.version('0.0.1')
+program.option('-f, --format [type]', 'output format')
+program.argument('<filepath1> <filepath2>')
 program.parse(process.argv);
