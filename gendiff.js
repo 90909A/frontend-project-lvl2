964ccd12667c960cc278038d40b09147099a3c1b
Katya <ktrnstm@gmail.com>
2021-12-08 15:55:27 +0300

    step(3-4)

--- gendiff.js
+++ gendiff.js
@@ -1,6 +1,15 @@
-const program = require('commander');
-program.description(' Compares two configuration files and shows a difference.')
-program.version('0.0.1')
-program.option('-f, --format [type]', 'output format')
-program.argument('<filepath1> <filepath2>')
-program.parse(process.argv);
+import { step3 } from './step3.js';
+import { program } from 'commander';
+
+//const program = require('commander');//
+
+program
+.description(' Compares two configuration files and shows a difference.')
+.version('0.0.1')
+.option('-f, --format [type]', 'output format')
+.argument('<filepath1>')
+.argument('<filepath2>')
+.action((filepath1, filepath2) => {
+  console.log(step3(filepath1,filepath2));
+  })
+.parse(process.argv);
